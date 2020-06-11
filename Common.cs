e8b82d39b82011b3d3880be366ece0ba0cdbe078
zuleyhaeker <60626948+zuleyhaeker@users.noreply.github.com>
2020-06-11 23:34:09 +0300

    Add files via upload

---
+++ Common.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LogoQuizGame.Common
+{
+    public class Common
+    {
+        public static char[] user_submit_answer;
+        public static string[] alphabet_characters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", };
+    }
+}
